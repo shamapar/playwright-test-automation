24de77119047fba5942ac9944d2276557a22076e
Shama <shamapar494@gmail.com>
2025-03-05 16:48:11 +1100

    practices for inputs

--- tests/registration.spec.js
+++ tests/registration.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('validate login', async({ page })=>{
+test('validate signup', async({ page })=>{
 
     await page.goto("https://parabank.parasoft.com/parabank/index.htm");
 
