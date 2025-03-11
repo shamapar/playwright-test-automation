f5d5ec1ad9b3bf2dd483af58d93564057f6bd491
Shama <shamapar494@gmail.com>
2025-03-11 19:02:33 +1100

    drop down and multidrop examples

---
+++ tests/dropbownboxspec..js
@@ -0,0 +1,32 @@
+const { test, expect } = require('@playwright/test');
+test('multidropdown testing', async ({ page }) => {
+    await page.goto("https://testautomationpractice.blogspot.com/");
+    await expect(page).toHaveURL("https://testautomationpractice.blogspot.com/");
+    await page.selectOption('#colors', ['Blue', 'Red', 'Yellow']);
+
+    // const opt = page.locator('#colors option');
+    // await expect(opt).toHaveCount(7);
+    // await expect(opt).toContainText(['Red', 'Blue']);
+
+    const locator = (await page.$$('#colors option'));//stored in array
+    console.log("All options", locator.length);
+    expect(locator.length).toEqual(7);
+
+    const options = await page.locator('#colors').textContent();
+    console.log(options);
+    await expect(options).toContain("Green");
+    await expect(options.includes('Blue')).toBeTruthy();
+
+    let test = false;
+    for (let option of locator) {
+
+        const val = await option.textContent();
+        if (val.includes("Blue")) {
+            test = true;
+            break;
+        }
+    }
+    await expect(test).toBeTruthy();
+
+})
+
